d0c5d28c04c97e3a80afdbb7ff61f7d2b47af40d
TuteCode <tuteapps@gmail.com>
2022-12-29 10:18:27 -0300

    update

--- Agencia_Marketing/core/settings.py
+++ Agencia_Marketing/core/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
+
 import environ
 
 env = environ.Env()
