1b9006cfb1a35c8fcc9e1e9582d2a8dd8172820d
TuteCode <tuteapps@gmail.com>
2023-01-02 23:53:25 -0300

    update

--- Agencia_Marketing/core/urls.py
+++ Agencia_Marketing/core/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
+urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
