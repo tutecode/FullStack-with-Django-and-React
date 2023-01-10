f75f69530c93d77e6ec4cf5345d8b42d9a2df1ff
TuteCode <tuteapps@gmail.com>
2023-01-10 01:02:41 -0300

    update

--- Agencia_Marketing/src/components/careers/Testimonial.js
+++ Agencia_Marketing/src/components/careers/Testimonial.js
@@ -1,8 +1,8 @@
-export default function Testimonial() {
+function Testimonial() {
     return (
         <div className="bg-white pt-16 lg:py-24">
-            <div className="bg-indigo-600 pb-16 lg:relative lg:z-10 lg:pb-0">
-                <div className="lg:mx-auto lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-8 lg:px-8">
+            <div className="bg-gray-50 pb-16 lg:relative lg:z-10 lg:pb-0">
+                <div className="lg:mx-12 lg:grid lg:max-w-full lg:grid-cols-3 lg:gap-8 lg:px-8">
                     <div className="relative lg:-my-8">
                         <div aria-hidden="true" className="absolute inset-x-0 top-0 h-1/2 bg-white lg:hidden" />
                         <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:h-full lg:p-0">
@@ -20,21 +20,21 @@ export default function Testimonial() {
                             <blockquote>
                                 <div>
                                     <svg
-                                        className="h-12 w-12 text-white opacity-25"
+                                        className="h-12 w-12 text-black opacity-25"
                                         fill="currentColor"
                                         viewBox="0 0 32 32"
                                         aria-hidden="true"
                                     >
                                         <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
                                     </svg>
-                                    <p className="mt-6 text-2xl font-medium text-white">
+                                    <p className="mt-6 text-2xl font-medium text-gray-500">
                                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet
                                         feugiat est integer dolor auctor adipiscing nunc urna, sit.
                                     </p>
                                 </div>
                                 <footer className="mt-6">
-                                    <p className="text-base font-medium text-white">Judith Black</p>
-                                    <p className="text-base font-medium text-indigo-100">CEO at PureInsights</p>
+                                    <p className="text-base font-medium text-black">Judith Black</p>
+                                    <p className="text-base font-medium text-gray-700">CEO at PureInsights</p>
                                 </footer>
                             </blockquote>
                         </div>
@@ -44,3 +44,4 @@ export default function Testimonial() {
         </div>
     )
 }
+export default Testimonial
--- Agencia_Marketing/src/components/navigation/Navbar.js
+++ Agencia_Marketing/src/components/navigation/Navbar.js
@@ -1,5 +1,7 @@
 import { connect } from "react-redux"
-import { useState } from 'react'
+import { Popover, Transition } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import { useState, Fragment, useEffect } from 'react'
 import { NavLink, Link } from 'react-router-dom'
 //import loading_dots from 'assets/img/loading-dots.gif'
 import logo_boomslag from 'assets/img/boomslag-black.png'
@@ -7,17 +9,38 @@ import logo_boomslag from 'assets/img/boomslag-black.png'
 import { DotLoader } from "react-spinners"
 //import PacmanLoader from "react-spinners/PacmanLoader"
 
+const solutions = [
+    {
+        name: 'Insights',
+        description: 'Measure actions your users take',
+        href: '##',
+        icon: IconOne,
+    },
+    {
+        name: 'Automations',
+        description: 'Create your own targeted content',
+        href: '##',
+        icon: IconTwo,
+    },
+    {
+        name: 'Reports',
+        description: 'Keep track of your growth',
+        href: '##',
+        icon: IconThree,
+    },
+]
+
 function Navbar() {
 
     // Loading icon
     const [loading, setLoading] = useState(true)
 
-    window.onscroll = function() {
+    window.onscroll = function () {
         scrollFunction()
     }
 
     function scrollFunction() {
-        if(document.getElementById('navbar')) {
+        if (document.getElementById('navbar')) {
             if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
                 document.getElementById('navbar').classList.add('shadow-navbar');
                 document.getElementById('navbar').classList.add('bg-white');
@@ -28,15 +51,16 @@ function Navbar() {
         }
     }
 
-
+    const [open, setOpen] = useState(false)
 
     return (
         <nav id='navbar' className='w-full py-2 top-0 transition duration-300 ease-in-out z-40 fixed'>
             <div className="px-4 sm:px-6">
-                <div className="-ml-4 -mt-2 flex flex-wrap items-center justify-between sm:flex-nowrap md:px-14 px-2">
+                <div className="-ml-4 -mt-2 hidden lg:flex flex-wrap items-center justify-between sm:flex-nowrap md:px-14 px-2">
                     <Link to='/' className="ml-4 mt-2">
                         <img src={logo_boomslag} width={180} height={90} className="" />
                     </Link>
+
                     <div className="ml-4 mt-2 flex-shrink-0">
 
                         <NavLink to='/casos' className="text-lg inline-flex font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-yellow-button transition duration-300 ease-in-out mx-4">Casos</NavLink>
@@ -54,6 +78,84 @@ function Navbar() {
                         </button>
                     </div>
                 </div>
+                <div className="-ml-4 -mt-2 lg:hidden flex flex-wrap items-center justify-between sm:flex-nowrap md:px-14 px-2">
+                    <Link to='/' className="ml-4 mt-2">
+                        <img src={logo_boomslag} width={180} height={90} className="" />
+                    </Link>
+
+                    <div className="ml-4 mt-2 flex-shrink-0">
+
+                        <Popover className="relative">
+                            {({ open }) => (
+                                <>
+                                    <Popover.Button
+                                        className={`
+                                            ${open ? '' : 'text-opacity-90'}
+                                            focus:ring-none focus:outline-none`}
+                                    >
+                                        {
+                                            open ?
+                                                <i className='bx bx-x text-4xl'></i>
+                                                :
+                                                <i className='bx bx-menu text-4xl'></i>
+                                        }
+
+                                    </Popover.Button>
+                                    <Transition
+                                        as={Fragment}
+                                        enter="transition ease-out duration-200"
+                                        enterFrom="opacity-0 translate-y-1"
+                                        enterTo="opacity-100 translate-y-0"
+                                        leave="transition ease-in duration-150"
+                                        leaveFrom="opacity-100 translate-y-0"
+                                        leaveTo="opacity-0 translate-y-1"
+                                    >
+                                        <Popover.Panel className="absolute -left-32 z-10 mt-3 w-screen max-w-sm -translate-x-1/2 transform px-4 sm:px-0 lg:max-w-3xl">
+                                            <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                                                <div className="relative grid gap-8 bg-white p-7 lg:grid-cols-2">
+                                                    {solutions.map((item) => (
+                                                        <a
+                                                            key={item.name}
+                                                            href={item.href}
+                                                            className="-m-3 flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
+                                                        >
+                                                            <div className="flex h-10 w-10 shrink-0 items-center justify-center text-white sm:h-12 sm:w-12">
+                                                                <item.icon aria-hidden="true" />
+                                                            </div>
+                                                            <div className="ml-4">
+                                                                <p className="text-sm font-medium text-gray-900">
+                                                                    {item.name}
+                                                                </p>
+                                                                <p className="text-sm text-gray-500">
+                                                                    {item.description}
+                                                                </p>
+                                                            </div>
+                                                        </a>
+                                                    ))}
+                                                </div>
+                                                <div className="bg-gray-50 p-4">
+                                                    <a
+                                                        href="##"
+                                                        className="flow-root rounded-md px-2 py-2 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
+                                                    >
+                                                        <span className="flex items-center">
+                                                            <span className="text-sm font-medium text-gray-900">
+                                                                Documentation
+                                                            </span>
+                                                        </span>
+                                                        <span className="block text-sm text-gray-500">
+                                                            Start integrating products and tools
+                                                        </span>
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </Popover.Panel>
+                                    </Transition>
+                                </>
+                            )}
+                        </Popover>
+                    </div>
+                </div>
             </div>
         </nav>
     )
@@ -66,3 +168,89 @@ const mapStateToProps = states => ({
 export default connect(mapStateToProps, {
 
 })(Navbar)
+
+
+
+
+
+
+
+
+function IconOne() {
+    return (
+        <svg
+            width="48"
+            height="48"
+            viewBox="0 0 48 48"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <rect width="48" height="48" rx="8" fill="#FFEDD5" />
+            <path
+                d="M24 11L35.2583 17.5V30.5L24 37L12.7417 30.5V17.5L24 11Z"
+                stroke="#FB923C"
+                strokeWidth="2"
+            />
+            <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M16.7417 19.8094V28.1906L24 32.3812L31.2584 28.1906V19.8094L24 15.6188L16.7417 19.8094Z"
+                stroke="#FDBA74"
+                strokeWidth="2"
+            />
+            <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M20.7417 22.1196V25.882L24 27.7632L27.2584 25.882V22.1196L24 20.2384L20.7417 22.1196Z"
+                stroke="#FDBA74"
+                strokeWidth="2"
+            />
+        </svg>
+    )
+}
+
+function IconTwo() {
+    return (
+        <svg
+            width="48"
+            height="48"
+            viewBox="0 0 48 48"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <rect width="48" height="48" rx="8" fill="#FFEDD5" />
+            <path
+                d="M28.0413 20L23.9998 13L19.9585 20M32.0828 27.0001L36.1242 34H28.0415M19.9585 34H11.8755L15.9171 27"
+                stroke="#FB923C"
+                strokeWidth="2"
+            />
+            <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M18.804 30H29.1963L24.0001 21L18.804 30Z"
+                stroke="#FDBA74"
+                strokeWidth="2"
+            />
+        </svg>
+    )
+}
+
+function IconThree() {
+    return (
+        <svg
+            width="48"
+            height="48"
+            viewBox="0 0 48 48"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <rect width="48" height="48" rx="8" fill="#FFEDD5" />
+            <rect x="13" y="32" width="2" height="4" fill="#FDBA74" />
+            <rect x="17" y="28" width="2" height="8" fill="#FDBA74" />
+            <rect x="21" y="24" width="2" height="12" fill="#FDBA74" />
+            <rect x="25" y="20" width="2" height="16" fill="#FDBA74" />
+            <rect x="29" y="16" width="2" height="20" fill="#FB923C" />
+            <rect x="33" y="12" width="2" height="24" fill="#FB923C" />
+        </svg>
+    )
+}
