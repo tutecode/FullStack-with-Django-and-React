df7c085358e0d2f2ba71070975fb62f7798e7fd1
TuteCode <tuteapps@gmail.com>
2023-01-15 12:20:04 -0300

    update

--- Blog_Personal/apps/category/views.py
+++ Blog_Personal/apps/category/views.py
@@ -1,13 +1,15 @@
-from rest_framework.views import APIView
+from rest_framework import permissions, status
 from rest_framework.response import Response
-from rest_framework import status
-from rest_framework import permissions
+from rest_framework.views import APIView
+
 from .models import Category
+
 
 class ListCategoriesView(APIView):
     """List Categories"""
 
-    permission_classes = (permissions.AllowAny,) # tuple: permisos de las personas
+    # tuple: permisos de las personas
+    permission_classes = (permissions.AllowAny,)
 
     def get(self, request, format=None):
         """Get HTTPS"""
@@ -20,22 +22,22 @@ class ListCategoriesView(APIView):
             for category in categories:
                 if not category.parent:
                     item = {}
-                    item['id']=category.id
-                    item['name']=category.name
-                    item['slug']=category.slug
-                    item['views']=category.views
-                
+                    item['id'] = category.id
+                    item['name'] = category.name
+                    item['slug'] = category.slug
+                    item['views'] = category.views
                     item['sub_categories'] = []
+
                     for sub_category in categories:
                         sub_item = {}
                         if sub_category.parent and sub_category.parent.id == category.id:
-                            sub_item['id']=sub_category.id
-                            sub_item['name']=sub_category.name
-                            sub_item['slug']=sub_category.slug
-                            sub_item['views']=sub_category.views
+                            sub_item['id'] = sub_category.id
+                            sub_item['name'] = sub_category.name
+                            sub_item['slug'] = sub_category.slug
+                            sub_item['views'] = sub_category.views
 
                             item['sub_categories'].append(sub_item)
-                    
+
                     result.append(item)
 
             return Response({'categories': result}, status=status.HTTP_200_OK)
