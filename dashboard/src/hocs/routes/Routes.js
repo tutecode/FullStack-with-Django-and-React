cdec28102a6037ebf1942bcbf4713e13263fa537
TuteCode <tuteapps@gmail.com>
2023-02-14 10:12:32 -0300

    update

--- dashboard/src/hocs/routes/Routes.js
+++ dashboard/src/hocs/routes/Routes.js
@@ -2,10 +2,11 @@ import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-route
 import { AnimatePresence } from 'framer-motion'
 import Error404 from 'containers/errors/Error404'
 import Home from 'containers/pages/Home'
-import Dashboard from 'containers/pages/blog/Dashboard'
+import Dashboard from 'containers/pages/Dashboard'
 import Blog from 'containers/pages/blog/Blog'
 import ResetPassword from 'containers/auth/ResetPassword'
 import ResetPasswordConfirm from 'containers/auth/ResetPasswordConfirm'
+import EditPost from 'containers/pages/blog/EditPost'
 
 export default function AnimatedRoutes() {
 
@@ -23,6 +24,7 @@ export default function AnimatedRoutes() {
             <Route path="/password/reset/confirm/:uid/:token" element={<ResetPasswordConfirm />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/blog" element={<Blog />} />
+            <Route path="/blog/:slug" element={<EditPost />} />
         </Routes>
         //</AnimatePresence>
     )
