bde3c3b855d0d0a97b0ac7848d1093148aaeefab
TuteCode <tuteapps@gmail.com>
2023-02-20 23:24:20 -0300

    update

--- Email/agencia/apps/contacts/urls.py
+++ Email/agencia/apps/contacts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    #path('', ContactCreateView.as_view()),
-    #path('opt-in', OptInView.as_view()),
+    path('', ContactCreateView.as_view()),
+    path('opt-in', OptInView.as_view()),
 ]
